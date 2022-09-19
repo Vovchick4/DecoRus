f8919c75530a460be657d51cb644250aafebb35d
Vovchick4 <vovaspodpivas@gmail.com>
2022-09-20 01:42:48 +0300

    fix

--- api/routes/home/controller.js
+++ api/routes/home/controller.js
@@ -1,6 +1,8 @@
 import routes from "../../../shared/config/routes.js"
 
 export const getHomePage = (req, res) => {
-    req.i18n.changeLanguage(req.url_lang);
+    const { lang } = req.cookies
+    const tempLng = req.query?.lng
+    req.i18n.changeLanguage(!tempLng ? lang || "ua" : tempLng)
     res.render('pages/home', { routes, pageId: req.url, activeLng: req.lng, full_url: req.url_lang })
 }   
