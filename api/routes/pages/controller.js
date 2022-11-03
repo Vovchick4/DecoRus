bbec08aedb83f487914bfa3a590c21ef47acb988
Vovchick4 <vovaspodpivas@gmail.com>
2022-11-03 17:12:39 +0200

    fix: admin pages

--- api/routes/pages/controller.js
+++ api/routes/pages/controller.js
@@ -11,7 +11,7 @@ export const getHomePage = async (req, res) => {
     const tempLng = req.query?.lng
     req.i18n.changeLanguage(!tempLng ? lang || "ua" : tempLng)
     res.render('pages/home', {
-        routes,
+        routes: routes.client,
         pageId: req.url.split("?")[0],
         activeLng: req.lng,
         full_url: req.url_lang,
@@ -24,15 +24,15 @@ export const getContactPage = (req, res) => {
     const { lang } = req.cookies
     const tempLng = req.query?.lng
     req.i18n.changeLanguage(!tempLng ? lang || "ua" : tempLng)
-    res.render('pages/contacts', { routes, pageId: req.url.split("?")[0], activeLng: req.lng, full_url: req.url_lang, base_url: process.env.BASE_URL })
+    res.render('pages/contacts', { routes: routes.client, pageId: req.url.split("?")[0], activeLng: req.lng, full_url: req.url_lang, base_url: process.env.BASE_URL })
 }
 
 export const getAdminPanelPage = (req, res) => {
-    res.render('pages/admin/admin-panel')
+    res.render('pages/admin/admin-panel', { routes: routes.admin, pageId: req.url.split("?")[0], full_url: req.url_lang, base_url: process.env.BASE_URL })
 }
 
 export const getCollectionsPage = async (req, res) => {
     const response = await fetch(`${process.env.BASE_URL}/api/collections`)
     const { collections } = await response.json()
-    res.render('pages/admin/collections', { collections })
+    res.render('pages/admin/collections', { routes: routes.admin, pageId: req.url.split("?")[0], full_url: req.url_lang, base_url: process.env.BASE_URL, collections })
 }
--- shared/config/routes.js
+++ shared/config/routes.js
@@ -1,12 +1,22 @@
-const routes = [
-    {
-        pathname: "/",
-        label: "Home page"
-    },
-    {
-        pathname: "/contacts",
-        label: "Contact page"
-    }
-]
+const routes = {
+    client: [
+        {
+            pathname: "/",
+            label: "Home page"
+        },
+        {
+            pathname: "/contacts",
+            label: "Contact page"
+        }],
+    admin: [
+        {
+            pathname: "/admin-panel-page",
+            label: "Admin Panel"
+        },
+        {
+            pathname: "/collections",
+            label: "Collections"
+        }]
+}
 
 export default routes
