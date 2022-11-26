a3aaf6aae4faf2608f9dbaa6633501aea10b2dd5
Vovchick4 <vovaspodpivas@gmail.com>
2022-11-26 12:09:57 +0200

    fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT || 8080
 
 i18next.use(i18nextMiddleware.LanguageDetector).use(Backend).init({
     backend: {
